e16d46da068905b2e7a53e7c36e0a8f3ec08f195
jooyeon <jooh7466@naver.com>
2023-08-16 20:56:04 +0900

    commit

--- src/main/java/com/example/demo/Project0816Application.java
+++ src/main/java/com/example/demo/Project0816Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project0630Application {
+public class Project0816Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project0630Application.class, args);
--- src/test/java/com/example/demo/Project0816ApplicationTests.java
+++ src/test/java/com/example/demo/Project0816ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Project0630ApplicationTests {
+class Project0816ApplicationTests {
 
 	@Test
 	void contextLoads() {
